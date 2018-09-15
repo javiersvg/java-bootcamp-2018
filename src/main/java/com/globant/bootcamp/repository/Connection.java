4f36ac0c0aceac9e15014efa0cfc18674be71be0
Rodrigo Grazini <grazini.rodrigo@gmail.com>
2018-09-15 18:48:36 +0000

    Grazini Rodrigo - J-B-18-Rodri-Grazi - Code formating, removed blank spaces, removed unnecessary comments, removed unnecessary private constructors in Connection Singletons

--- src/main/java/com/globant/bootcamp/repository/Connection.java
+++ src/main/java/com/globant/bootcamp/repository/Connection.java
@@ -1,24 +1,22 @@
 package com.globant.bootcamp.repository;
+
 /**
  * Abstract stub class for a database connection
  */
 public abstract class Connection {
-    
-    protected String dbUrl;
-    protected Boolean status;
-    
-    /**
-     * @return status: If connection with the database is established 
-     */
-    public boolean getStatus() {
-        return status;
-    }
-    
-    
-    /**
-     * @return dbUrl: URL of the database
-    */
-    public String getUrl() {
-        return dbUrl;
-    }
+
+	protected String dbUrl;
+
+	protected boolean status;
+
+	/**
+	 * @return status: If connection with the database is established 
+	 */
+	public boolean getStatus() {
+		return status;
+	}
+
+	public String getUrl() {
+		return dbUrl;
+	}
 }
