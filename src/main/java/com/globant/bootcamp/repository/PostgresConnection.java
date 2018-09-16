cdf7b1285712e282f217a06e51bb5fc1f3762881
Rodrigo Grazini <grazini.rodrigo@gmail.com>
2018-09-16 16:20:13 +0000

    Rodrigo Grazini - J-B-18-Rodri-Grazi - Changes connection behaviour, adds log4j support and adds resource file for reading pre-defined credentials(file resources/credentials.properties)
    # If applied, this commit will...
    Add a connect method to a Connection which takes the credentials for the db user and 'creates the session'. Add logging support using log4j
    Add properties files for reading user and password defined outside the code with testing purposes
    
    # Why is this change needed?
    Prior to this change, the Connections constructors had the responsiblty to make the connection, constructor should never have complicated logic inside them.
    Logging was made using standard output inside the code
    
    # How does it adress the issue?
    The connection now is handled with a method, constructor only sets up the database URL
    Logging is delegated to log4j

--- src/main/java/com/globant/bootcamp/repository/PostgresConnection.java
+++ src/main/java/com/globant/bootcamp/repository/PostgresConnection.java
@@ -1,25 +1,31 @@
 package com.globant.bootcamp.repository;
 
+import org.apache.log4j.Logger;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 /**
  * Stub class for a "postgres database connection" with static user and password authentication
  */
 public class PostgresConnection extends Connection {
+    
+    final static Logger logger = Logger.getLogger(PostgresConnection.class);
 
-	private static final String PG_ADMIN_USER = "admin";
-	private static final String PG_ADMIN_PASSWORD = "admin";
-
-	public PostgresConnection(String dbName, String user, String password) {
-		super.dbUrl = "jdbc:postgres:" + dbName;
-		super.status = false;
-
-		if (!PG_ADMIN_USER.equals(user)) {
-			return;
-		}
-
-		if (!PG_ADMIN_PASSWORD.equals(password)) {
-			return;
-		}
-
-		super.status = true;
+	public PostgresConnection(String dbName) {
+		super("jdbc:postgres:" + dbName);
 	}
+	
+	@Override
+	public void connect(Properties credentials) {
+	    ResourceBundle labels = ResourceBundle.getBundle(CREDENTIALS);
+	    String user = labels.getString("postgres.user");
+	    String password = labels.getString("postgres.password");
+	    logger.info("Using stub postgres connection class\n Connecting to mysql server with adress; "+this.getUrl());
+	    this.updateStatus(user.equals(credentials.get("user")) && password.equals(credentials.get("password")));
+	    if (this.getStatus()){
+	        logger.info("User "+credentials.get("user")+ " connected successfully");
+	    } else {
+	        logger.error("Something went wrong while connecting, incorrect user or password?");
+	    }
+    }
 }
