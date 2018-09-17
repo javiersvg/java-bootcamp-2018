cb526876df01f443d60bd855927360112cbccfc4
Rodrigo Grazini <grazini.rodrigo@gmail.com>
2018-09-17 03:54:59 +0000

    Rodrigo Grazini - J-B-18-Rodri-Grazi - Define Connection class as a contract

--- src/main/java/com/globant/bootcamp/repository/PostgresConnection.java
+++ src/main/java/com/globant/bootcamp/repository/PostgresConnection.java
@@ -1,31 +1,50 @@
 package com.globant.bootcamp.repository;
 
 import org.apache.log4j.Logger;
+
 import java.util.Properties;
 import java.util.ResourceBundle;
 
 /**
  * Stub class for a "postgres database connection" with static user and password authentication
  */
-public class PostgresConnection extends Connection {
+public class PostgresConnection implements Connection {
     
     final static Logger logger = Logger.getLogger(PostgresConnection.class);
+    
+    private String dbUrl;
+
+	private boolean status;
 
 	public PostgresConnection(String dbName) {
-		super("jdbc:postgres:" + dbName);
+
+		if (dbName == null) {
+			throw new IllegalArgumentException("Database URL can't be null");
+		}
+		this.dbUrl = "jdbc:postgres:" + dbName;
 	}
 	
 	@Override
 	public void connect(Properties credentials) {
-	    ResourceBundle labels = ResourceBundle.getBundle(CREDENTIALS);
+	    ResourceBundle labels = ResourceBundle.getBundle("credentials");
 	    String user = labels.getString("postgres.user");
 	    String password = labels.getString("postgres.password");
 	    logger.info("Using stub postgres connection class\n Connecting to mysql server with adress; "+this.getUrl());
-	    this.updateStatus(user.equals(credentials.get("user")) && password.equals(credentials.get("password")));
+	    this.status = user.equals(credentials.get("user")) && password.equals(credentials.get("password"));
 	    if (this.getStatus()){
 	        logger.info("User "+credentials.get("user")+ " connected successfully");
 	    } else {
 	        logger.error("Something went wrong while connecting, incorrect user or password?");
 	    }
+    }
+    
+    @Override
+    public boolean getStatus() {
+        return status;
+    }
+    
+    @Override
+    public String getUrl() {
+        return dbUrl;
     }
 }
--- src/main/java/com/globant/bootcamp/repository/SqlConnection.java
+++ src/main/java/com/globant/bootcamp/repository/SqlConnection.java
@@ -1,31 +1,49 @@
 package com.globant.bootcamp.repository;
 
 import org.apache.log4j.Logger;
+
 import java.util.Properties;
 import java.util.ResourceBundle;
 
 /**
  * Stub class for a "sql database connection" with static user and password authentication
  */
-public class SqlConnection extends Connection {
+public class SqlConnection implements Connection {
 
     final static Logger logger = Logger.getLogger(SqlConnection.class);
 
+	private String dbUrl;
+
+	private boolean status;
+
 	public SqlConnection(String dbName) {
-		super("jdbc:mysql:" + dbName);
+		if (dbName == null) {
+			throw new IllegalArgumentException("Database URL can't be null");
+		}
+		this.dbUrl = "mysql:postgres:" + dbName;
 	}
 	
 	@Override
 	public void connect(Properties credentials) {
-	    ResourceBundle labels = ResourceBundle.getBundle(CREDENTIALS);
+	    ResourceBundle labels = ResourceBundle.getBundle("credentials");
 	    String user = labels.getString("mysql.user");
 	    String password = labels.getString("mysql.password");
 	    logger.info("Using stub mysql connection class\n Connecting to mysql server with adress; "+this.getUrl());
-	    this.updateStatus(user.equals(credentials.get("user")) && password.equals(credentials.get("password")));   
+	    this.status = user.equals(credentials.get("user")) && password.equals(credentials.get("password"));   
 	    if (this.getStatus()){
 	        logger.info("User "+credentials.get("user")+ " connected successfully");
 	    } else {
 	        logger.error("Something went wrong while connecting, incorrect user or password?");
 	    }
+    }
+    
+    @Override
+    public boolean getStatus() {
+        return this.status;
+    }
+    
+    @Override
+    public String getUrl() {
+        return this.dbUrl;
     }
 }
