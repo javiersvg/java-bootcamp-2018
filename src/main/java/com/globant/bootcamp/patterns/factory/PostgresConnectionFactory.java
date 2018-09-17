f7a36c1adc761f35e7578848c63e8d6d3720e498
Rodrigo Grazini <grazini.rodrigo@gmail.com>
2018-09-17 20:50:54 +0000

    Rodrigo Grazini - J-B-18-Rodri-Grazi - Changed new Connection Factory instances for instances given by singletons classes

--- src/main/java/com/globant/bootcamp/patterns/factory/PostgresConnectionFactory.java
+++ src/main/java/com/globant/bootcamp/patterns/factory/PostgresConnectionFactory.java
@@ -1,11 +1,12 @@
 package com.globant.bootcamp.patterns.factory;
 
+import com.globant.bootcamp.patterns.sinlgeton.PostgresConnectionSingleton;
 import com.globant.bootcamp.repository.PostgresConnection;
 import com.globant.bootcamp.repository.Connection;
 
 public class PostgresConnectionFactory extends ConnectionAbstractFactory {
     @Override
     public Connection getConnection(String dbName) {
-        return new PostgresConnection(dbName);
+        return PostgresConnectionSingleton.getPostgresInstance(dbName);
     }
 }
--- src/main/java/com/globant/bootcamp/patterns/factory/SqlConnectionFactory.java
+++ src/main/java/com/globant/bootcamp/patterns/factory/SqlConnectionFactory.java
@@ -1,11 +1,11 @@
 package com.globant.bootcamp.patterns.factory;
 
-import com.globant.bootcamp.repository.SqlConnection;
+import com.globant.bootcamp.patterns.sinlgeton.SqlConnectionSingleton;
 import com.globant.bootcamp.repository.Connection;
 
 public class SqlConnectionFactory extends ConnectionAbstractFactory {
     @Override
     public Connection getConnection(String dbName) {
-       return new SqlConnection(dbName);
+        return SqlConnectionSingleton.getSqlInstance(dbName);
     }
 }
